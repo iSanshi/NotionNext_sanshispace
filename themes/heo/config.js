9c4208bd712ebf9802ac6038efbeb13f8720b802
iSanshi <63285503+iSanshi@users.noreply.github.com>
2024-03-15 16:30:19 -0700

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -24,9 +24,9 @@ const CONFIG = {
   HEO_HERO_TITLE_LINK: '',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '正在上学', url: '/gotoschool' },
-  HEO_HERO_CATEGORY_2: { title: '正在吃饭', url: '/food' },
-  HEO_HERO_CATEGORY_3: { title: '正在运动', url: '/exercising' },
+  HEO_HERO_CATEGORY_1: { title: '在上学', url: '/gotoschool' },
+  HEO_HERO_CATEGORY_2: { title: '在吃饭', url: '/food' },
+  HEO_HERO_CATEGORY_3: { title: '在运动', url: '/exercising' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
